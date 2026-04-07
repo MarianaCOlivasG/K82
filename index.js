69ede7d8d7460d9226fc5f79409caac0443a5966
MOlivasG <maolivas@pamhotels.com>
2026-04-06 19:10:44 -0500

    env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 app.listen( 3000, '0.0.0.0', () => {
-    console.log("Hola mundo");
+    console.log(`Hola ${ process.env.MI_NOMBRE } ${ process.env.MI_APELLIDO }`);
 })
 
 // node index.js
